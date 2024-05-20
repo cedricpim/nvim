aeb43a79995a245d32440f4adb39b8151088ea2e
Cedric Pimenta <cedric@teampeanut.com>
2024-05-20 14:05:00 +0100

    Enable treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    {
+      "andymass/vim-matchup",
+      init = function() vim.g.matchup_matchparen_deferred = 1 end,
+    },
+  },
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
