2359253a21ae24d5e9194019f5491d0187ed77bc
Cedric Pimenta <cedric@teampeanut.com>
2024-05-20 15:13:56 +0100

    Add mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -64,11 +64,31 @@ return {
         ["<Leader>b"] = { desc = "Buffers" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+        ["<leader>bD"] = {
+          function()
+            require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+          end,
+          desc = "Pick to close",
+        },
+        ["<leader>b"] = { name = "Buffers" },
+        ["<leader>pf"] = { function() require("user.utils").copy(vim.fn.expand("%:p")) end, desc = "Yank full path" },
+        ["<leader>pF"] = { function() require("user.utils").copy(vim.fn.expand("%:p") .. ":" .. vim.fn.line(".")) end, desc = "Yank full path with line" },
+        ["<leader>pr"] = { function() require("user.utils").copy(vim.fn.expand("%")) end, desc = "Yank relative path" },
+        ["<leader>pR"] = { function() require("user.utils").copy(vim.fn.expand("%") .. ":" .. vim.fn.line(".")) end, desc = "Yank relative path with line" },
+        ["<leader>bX"] = { function() require("astronvim.utils.buffer").close_all(false, false) end, desc = "Close all buffers without saving" },
+        ["<leader>m"] = { "<cmd>MundoToggle<cr>", desc = "MundoToggle" },
+        ["<leader>fj"] = { "<cmd>AnyJump<cr>", desc = "Show definition references" },
       },
       t = {
         -- setting a mapping to false will disable it
         -- ["<esc>"] = false,
       },
+      x = {
+        ["v"] = { '<Plug>(expand_region_expand)' },
+        ["V"] = { '<Plug>(expand_region_shrink)' },
+        ["<leader>fj"] = { "<cmd>AnyJump<cr>", desc = "Show definition references" },
+      }
     },
   },
 }
